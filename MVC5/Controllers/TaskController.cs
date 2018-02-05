c899837b9f13be21e251bb6e1e1b6bd85bf778c1
mateus <mateus@mateusribeirooliveira.com>
2018-02-05 18:30:33 -0300

    Init

---
+++ MVC5/Controllers/TaskController.cs
@@ -0,0 +1,113 @@
+﻿using MVC5.Models;
+using MVC5.App_Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace MVC5.Controllers
+{
+    public class TaskController : Controller
+    {
+        string baseUrl = ConfigurationManager.AppSettings["serviceBaseURL"];
+
+        public async Task<ActionResult> Index()
+        {
+            RESTClient client = new RESTClient(baseUrl);
+
+            var response = await client.Get<IEnumerable<Models.Task>>("api/Task");
+
+            return View(response);
+        }
+
+        // GET: Test/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            RESTClient client = new RESTClient(baseUrl);
+
+            var response = await client.Get<Models.Task>($"api/Task/{id}");
+
+            return View(response);
+        }
+
+        // GET: Test/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Test/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(Models.Task model)
+        {
+            try
+            {
+                RESTClient client = new RESTClient(baseUrl);
+                await client.PostJson<string>("api/Task", model);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+        // GET: Test/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            RESTClient client = new RESTClient(baseUrl);
+
+            var response = await client.Get<Models.Task>($"api/Task/{id}");
+
+            return View(response);
+        }
+
+        // POST: Test/Edit/5
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, Models.Task model)
+        {
+            try
+            {
+                RESTClient client = new RESTClient(baseUrl);
+                await client.PutJson<string>($"api/Task/{id}", model);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Test/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            RESTClient client = new RESTClient(baseUrl);
+
+            var response = await client.Get<Models.Task>($"api/Task/{id}");
+
+            return View(response);
+        }
+
+        // POST: Test/Delete/5
+        [HttpPost]
+        public async Task<ActionResult> Delete(int id, Models.Task model)
+        {
+            try
+            {
+                RESTClient client = new RESTClient(baseUrl);
+                await client.Delete<string>($"api/Task/{id}");
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC5/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public string Owner { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
